c17e6921a463d5331ef0ad149c453aeb73303bae
Håkon Sørensen <haakonsorensen89@gmail.com>
2025-04-01 18:29:56 +0200

    changed back

--- index.ts
+++ index.ts
@@ -1,31 +1,7 @@
-const people = [
-  { firstname: 'Magnus', lastname: 'Magnusson' },
-  { firstname: 'Stefan', lastname: 'Stefanson' },
-]
-
-export const greeting = (firstname: string, lastname: string) => {
-  console.log(`Hei hei ${firstname} ${lastname}`);
-}
-const people = [
-  { firstname: 'Magnus', lastname: 'Magnusson' },
-  { firstname: 'Stefan', lastname: 'Stefanson' },
-]
-
-export const greeting = (firstname: string, lastname: string) => {
-  console.log(`Hei hei ${firstname} ${lastname}`);
-}
-
-const people = [
-  { firstname: 'Magnus', lastname: 'Magnusson' },
-  { firstname: 'Stefan', lastname: 'Stefanson' },
-]
-
-export const greeting = (firstname: string, lastname: string) => {
-  console.log(`Hei hei ${firstname} ${lastname}`);
-}
-
-export const greetTheGang = () => {
-  people.forEach((person) => {
-    greeting(person.firstname, person.lastname);
-  });
-}
+/**
+ * Function that greets a person
+ * @param name Name of person
+ */
+export const greeting = (name: string) => {
+  console.log(`Hello ${name}`);
+};
